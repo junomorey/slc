958b69461ca840f6df4fa4ab3481cab2b5122187
Lexie Polevoi <apolevoi@berkeley.edu>
2017-10-28 04:26:25 +0000

    Rspec tests for tutees_controller

--- spec/controllers/tutees_controller_spec.rb
+++ spec/controllers/tutees_controller_spec.rb
@@ -21,18 +21,18 @@ RSpec.describe TuteesController, type: :controller do
     end
     
 
-    describe 'validate_params acting as expected' do
-        it 'should throw an argument error if no first name' do
-        tutee = instance_double("Tutee", :email => "apple@gmail.com", :password => "example", 
-        :password_confirmation => "example", :first_name => "Sally", :sid => "1234", :grade => 
-        "Freshman", :phone_number => "415-123-4567", :semesters_at_cal => "4", :major => "Biology",
-        :requested_class => "CS61A", :in_dsp => "Yes")
-        #tutee.should_receive(:validate_params).and_return(ArgumentError)
+    # describe 'validate_params acting as expected' do
+    #     it 'should throw an argument error if no first name' do
+    #     tutee = instance_double("Tutee", :email => "apple@gmail.com", :password => "example", 
+    #     :password_confirmation => "example", :first_name => "Sally", :sid => "1234", :grade => 
+    #     "Freshman", :phone_number => "415-123-4567", :semesters_at_cal => "4", :major => "Biology",
+    #     :requested_class => "CS61A", :in_dsp => "Yes")
+    #     #tutee.should_receive(:validate_params).and_return(ArgumentError)
         
 
-        tutee.validate_params
-        end
-    end
+    #     tutee.validate_params
+    #     end
+    # end
         
     
      describe 'fill in attributes' do
@@ -41,5 +41,16 @@ RSpec.describe TuteesController, type: :controller do
             expect(tutee).to have_attributes(:email => "apple@gmail.com", :password => "example", :password_confirmation => "example", :first_name => "Sally", :last_name => "Sue")
         end
     end
+    
+ 
+    # describe 'tutor match method works', :type => :controller do 
+    #     it 'should match a tutor to a tutee' do
+    #     tutor = instance_double("Tutor", :email => "apple@gmail.com", :password => "example", :password_confirmation => "example", :first_name => "Sally", :last_name => "Sue")
+    #     tutee = instance_double("Tutee", :email => "apple@gmail.com", :password => "example", :password_confirmation => "example", :first_name => "Sally", :last_name => "Sue")
+    #     expect {get :tutor_match, :id => "1"}.to change(Tutor, :count).by(0)
+    #     expect(tutee.tutor_match).to equal(tutor)
+    #     tutee.tutor_match
+    #     end
+    # end
 end
 
