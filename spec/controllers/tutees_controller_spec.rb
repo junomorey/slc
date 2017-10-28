2a706778fd64f59adcb543c39a9c5af9b84373b6
Lexie Polevoi <apolevoi@berkeley.edu>
2017-10-28 03:48:35 +0000

    figuring out how to write rspec for tutees_controller

--- spec/controllers/tutees_controller_spec.rb
+++ spec/controllers/tutees_controller_spec.rb
@@ -14,6 +14,27 @@ RSpec.describe TuteesController, type: :controller do
         end
     end
     
+    describe "routes /tutees/1/tutor_match to correct path", :type => :routing do
+        it 'routes to tutor_match route' do
+            expect(get("tutees/1/tutor_match/")).to route_to("tutees#tutor_match", :id => "1")
+        end
+    end
+    
+
+    describe 'validate_params acting as expected' do
+        it 'should throw an argument error if no first name' do
+        tutee = instance_double("Tutee", :email => "apple@gmail.com", :password => "example", 
+        :password_confirmation => "example", :first_name => "Sally", :sid => "1234", :grade => 
+        "Freshman", :phone_number => "415-123-4567", :semesters_at_cal => "4", :major => "Biology",
+        :requested_class => "CS61A", :in_dsp => "Yes")
+        #tutee.should_receive(:validate_params).and_return(ArgumentError)
+        
+
+        tutee.validate_params
+        end
+    end
+        
+    
      describe 'fill in attributes' do
         it 'should have a filled in first and last name attribute' do
             tutee = instance_double("Tutee", :email => "apple@gmail.com", :password => "example", :password_confirmation => "example", :first_name => "Sally", :last_name => "Sue")
@@ -21,3 +42,4 @@ RSpec.describe TuteesController, type: :controller do
         end
     end
 end
+
